787fb7d8e6139e853264720b9e04c40d77762dd7
Robelashame <rasha679@mtroyal.ca>
2026-08-25 22:59:31 -0600

    added test and made it work with the test.log

--- logparser.py
+++ logparser.py
@@ -5,23 +5,30 @@ def parse_ssh_log(log_line: str) -> dict:
     timestamp = splitline[0] + " " + splitline[1] + " " + splitline[2]
     host = splitline[3]
 
+    event = ""
+
     # Make event detection function later on
     if "Accepted" in log_line or "session opened" in log_line:
         event = "SUCCESS_LOGIN"
-    elif "Failed password" in log_line or "Invalid user" in log_line:
+    elif "Failed password" in log_line or "Invalid user" in log_line or "authentication failures" in log_line:
         event = "FAILED_LOGIN"
     
     user = ""
     
     count = 0
-    for word in splitline:
-        if word == "user":
-            user = splitline[count + 1]
-            break
-        elif word == "for":
-            user = splitline[count + 1]
-            break
-        count += 1
+    if " user " in log_line:
+        for word in splitline:
+            if word == "user":
+                user = splitline[count + 1]
+                break
+            count += 1
+    
+    elif " for " in log_line:
+        for word in splitline:
+            if word == "for":
+                user = splitline[count + 1]
+                break
+            count += 1
 
     if user == "":
         user = "NO_USER"
@@ -31,6 +38,7 @@ def parse_ssh_log(log_line: str) -> dict:
     for word in splitline:
         if word == "from":
             ip = splitline[count + 1]
+            break
         count += 1
 
 
@@ -45,23 +53,13 @@ def parse_ssh_log(log_line: str) -> dict:
     return log_entry
 
 def parse_line(log_line: str) -> dict:
-
-    splitline = log_line.split()
-    timestamp = splitline[0] + " " + splitline[1] + " " + splitline[2]
-    event = splitline[3]
-    user = splitline[4].replace("user=", "")
-    ip = splitline[5].replace("ip=", "")
-
-    log_entry = {
-        "timestamp": timestamp,
-        "event": event, 
-        "user": user, 
-        "ip": ip
-    }
-
-    return log_entry
-
-
+    if "sshd[" in log_line:
+        return parse_ssh_log(log_line)
+    elif "sudo" in log_line:
+        #Put sudo function here
+        return {}
+    else:
+        return {}
 
 def parse_log(log_file: str) -> list[dict]:
 
@@ -72,4 +70,9 @@ def parse_log(log_file: str) -> list[dict]:
             parsed_log.append(parse_line(line))
     
     return parsed_log
+
+def main() -> None:
+    print(parse_log("test.log"))
+    
+main()
 
