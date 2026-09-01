089a2b675184c97dd2ec1bd289ed1eab6c83eba1
Robelashame <rasha679@mtroyal.ca>
2026-09-01 02:17:36 -0600

    Added event detection, parse user and ip function, and ssh brute force detection. Added mac DS store to gitignore

--- logparser.py
+++ logparser.py
@@ -1,50 +1,66 @@
+from collections import deque
 
+
+def event_ssh_detection(log_line: str) -> str:
+
+    event = ""
+
+    if "Accepted password" in log_line or "Accepted passkey" in log_line:
+        event = "SUCCESS_LOGIN"
+    elif "session opened" in log_line:
+        event = "SESSION_OPENED"
+    elif "Failed password" in log_line:
+        event = "FAILED_LOGIN"
+    elif "authentication failure" in log_line:
+        event = "FAILED_AUTHENTICATION"
+    elif "Invalid user" in log_line:
+        event = "INVALID_USER"
+    elif "Connection closed" in log_line:
+        event = "PREAUTH_CONNECTION_CLOSED"
+    elif "session closed" in log_line:
+        event = "SESSION_CLOSED"
+
+    return event
+
+def parse_ssh_fields(splitline: list[str], event: str) -> tuple[str, str]:
+
+    user = "NO_USER"
+    ip = "NO_IP"
+
+    if event == "SUCCESS_LOGIN":
+        user = splitline[splitline.index("for") + 1]
+        ip = splitline[splitline.index("from") + 1]
+    elif event == "SESSION_OPENED":
+        user = splitline[splitline.index("user") + 1].split(")", 1)[0]
+    elif event == "FAILED_LOGIN":
+        user = splitline[splitline.index("for") + 1]
+        ip = splitline[splitline.index("from") + 1]
+    elif event == "FAILED_AUTHENTICATION":
+        for word in splitline:
+            if word.startswith("rhost="):
+                ip = word.split("=", 1)[1]
+            elif word.startswith("user="):
+                user = word.split("=", 1)[1]
+    elif event == "INVALID_USER":
+        user = splitline[splitline.index("user") + 1]
+        ip = splitline[splitline.index("from") + 1]
+    elif event == "SESSION_CLOSED":
+        user = splitline[splitline.index("user") + 1]
+    
+    return user, ip
 
 def parse_ssh_log(log_line: str) -> dict:
     splitline = log_line.split()
     timestamp = splitline[0] + " " + splitline[1] + " " + splitline[2]
     host = splitline[3]
 
-    event = ""
-
-    # Make event detection function later on
-    if "Accepted" in log_line or "session opened" in log_line:
-        event = "SUCCESS_LOGIN"
-    elif "Failed password" in log_line or "Invalid user" in log_line or "authentication failures" in log_line:
-        event = "FAILED_LOGIN"
-    
-    user = ""
-    
-    count = 0
-    if " user " in log_line:
-        for word in splitline:
-            if word == "user":
-                user = splitline[count + 1]
-                break
-            count += 1
-    
-    elif " for " in log_line:
-        for word in splitline:
-            if word == "for":
-                user = splitline[count + 1]
-                break
-            count += 1
-
-    if user == "":
-        user = "NO_USER"
-
-    ip = "NO_IP"
-    count = 0
-    for word in splitline:
-        if word == "from":
-            ip = splitline[count + 1]
-            break
-        count += 1
-
+    event = event_ssh_detection(log_line)
+    user, ip = parse_ssh_feilds(splitline, event)
 
     log_entry = {
         "timestamp": timestamp,
         "host": host,
+        "source": "ssh",
         "event": event, 
         "user": user, 
         "ip": ip
@@ -53,7 +69,7 @@ def parse_ssh_log(log_line: str) -> dict:
     return log_entry
 
 def parse_line(log_line: str) -> dict:
-    if "sshd[" in log_line:
+    if "sshd" in log_line:
         return parse_ssh_log(log_line)
     elif "sudo" in log_line:
         #Put sudo function here
@@ -71,8 +87,70 @@ def parse_log(log_file: str) -> list[dict]:
     
     return parsed_log
 
+def get_log_mins(log: dict) -> int:
+
+    timestamp = log["timestamp"]
+
+    time = timestamp.split(" ")[2]
+    hours = int(time.split(":")[0])
+    minutes = int(time.split(":")[1])
+    
+    return (hours * 60) + minutes
+
+def ssh_brute_force_detect(logs: list[dict]) -> list[dict]:
+    failed_login = {}
+    alerts = []
+    danger_ips = set()
+    for log in logs:
+        if log["event"] == "FAILED_LOGIN":
+            ip = log["ip"]
+            if ip not in failed_login:
+                failed_login[ip] = deque()
+            failed_login[ip].append(log)
+            
+            if len(failed_login[ip]) >= 5:
+                if get_log_mins(log) - get_log_mins(failed_login[ip][0]) < 5:
+                    if ip not in danger_ips:
+                        danger_ips.add(ip)
+
+                        alerts.append({
+                            "alert": "SSH_BRUTE_FORCE",
+                            "ip": ip,
+                            "attempts": len(failed_login[ip]),
+                            "start_time": failed_login[ip][0]["timestamp"],
+                            "end_time": failed_login[ip][-1]["timestamp"]
+                        })
+
+                else:
+                    while get_log_mins(log) - get_log_mins(failed_login[ip][0]) >= 5:
+                        failed_login[ip].popleft()
+
+    return alerts
+
+#def ssh_user_enumeration_detect(logs: list[dict]) -> list[dict]:
+
+
+#def ssh_alert_detection(logs: list[dict]) -> dict:
+
+                
+
+
+
+def alert_detection(logs: list[dict]) -> str:
+    
+    alert = ""
+    
+    if logs[0]["source"] == "ssh":
+        alert = ssh_alert_detection(logs)
+    
+
+    return alert
+
+
+
+
 def main() -> None:
-    print(parse_log("test.log"))
+    print(ssh_brute_force_detect(parse_log("test.log")))
     
 main()
 
